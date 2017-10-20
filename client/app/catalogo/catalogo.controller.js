3985280ef8c38bb2cf82f823d830c99fe06c1059
jonathanbr1984 <jbolanos10@misena.edu.co>
2017-10-20 13:25:05 -0500

    modal

--- client/app/catalogo/catalogo.controller.js
+++ client/app/catalogo/catalogo.controller.js
@@ -1,16 +1,19 @@
 'use strict';
 (function() {
-	class CatalogoController {
-		constructor(){}
-		$onInit(){
-			$('#modalOferta').modal();
-		}
-
-	}
-	angular.module('bootstrapApp')
-	.component('catalogo', {
-		templateUrl: 'app/catalogo/catalogo.html',
-		controller: CatalogoController,
-		controllerAs: 'vm'
-	});
+    class CatalogoController {
+        constructor() {}
+        $onInit() {
+            if (!localStorage.respuestaModal)
+                $('#modalOferta').modal();
+        }
+        noMostrarModal() {
+            localStorage.respuestaModal = true;
+        }
+    }
+    angular.module('bootstrapApp')
+        .component('catalogo', {
+            templateUrl: 'app/catalogo/catalogo.html',
+            controller: CatalogoController,
+            controllerAs: 'vm'
+        });
 })();
--- client/app/catalogo/catalogo.js
+++ client/app/catalogo/catalogo.js
@@ -1,4 +1,4 @@
-'use strict',
+'use strict';
 angular.module('bootstrapApp')
 .config(function($stateProvider) {
 	$stateProvider
